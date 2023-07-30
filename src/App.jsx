088e55cb70730f58e35f4320b64d6ff9ed01e5f1
jees <jeesmon@cedextech.com>
2023-07-30 13:53:17 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import CursorFollower from "./components/CursorFollower";
+// import CursorFollower from "./components/CursorFollower";
 
 function App() {
   const [count, setCount] = useState(0);
