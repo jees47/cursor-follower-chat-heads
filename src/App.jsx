7d434791c21163f4a43cc841f2589668e89fc7da
jees <jeesmon@cedextech.com>
2023-07-30 13:56:28 +0530

    deployment updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-// import CursorFollower from "./components/CursorFollower";
+import CursorFollower from "./components/CursorFollower";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="bg-image">
-      {/* <CursorFollower /> */}
+      <CursorFollower />
     </div>
   );
 }
